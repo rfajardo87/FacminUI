7be4c0cbd835ba7550b153ce027ca1b9b2407cb6
Ruben Fajardo <rfaiard@gmail.com>
2023-01-26 20:26:49 -0600

    pointing to local

--- src/routes/main/clientes/+page.ts
+++ src/routes/main/clientes/+page.ts
@@ -4,14 +4,14 @@ import { personas as PersonaUri } from '../../../shared/API/endpoints/persona';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-	let personas: Persona[] = [];
-	try {
-		const rsp = await axiosInstance.get(PersonaUri);
-		personas = rsp.data.data as Persona[];
-	} catch (error) {
-		console.log(error);
-	}
-	return {
-		personas
-	};
+  let personas: Persona[] = [];
+  try {
+    const rsp = await axiosInstance.get(PersonaUri);
+    personas = rsp.data.data as Persona[];
+  } catch (error) {
+    console.log(error);
+  }
+  return {
+    personas
+  };
 }) satisfies PageLoad;
--- src/shared/API/instance.ts
+++ src/shared/API/instance.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
-import { NODE_ENV } from '$env/static/private';
 
 export const axiosInstance = axios.create({
-  baseURL:
-    NODE_ENV == 'prod' ? 'http://localhost:5000' : 'https://localhost:5001/',
+  baseURL: 'http://localhost:5000',
   headers: {}
 });
